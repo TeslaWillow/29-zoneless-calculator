82add5eed41177da4faffcae78836881b3fbaeaf
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-12-17 20:40:35 -0600

    calculator added

---
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator-view',
+  imports: [],
+  templateUrl: './calculator-view.component.html',
+  styles: ``
+})
+export default class CalculatorViewComponent {
+
+}
